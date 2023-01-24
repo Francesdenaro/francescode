3a617b4d17a961029a235ea050b5325746f13330
Francesco <fdenaro96@gmail.com>
2023-01-24 16:14:58 +0100

    Update with sitemaps and robots.txt

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const redirections = require('./redirections')
+
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
@@ -12,36 +14,7 @@ const nextConfig = {
 		],
 	},
 	async redirects() {
-		return [
-			{
-				source: '/6-types-of-developers-to-choose-your-learning-path/',
-				destination:
-					'/posts/6-types-of-developers-to-choose-your-learning-path/',
-				permanent: true,
-			},
-			{
-				source: '/5-easy-steps-to-become-a-self-taught-developer/',
-				destination: '/posts/5-easy-steps-to-become-a-self-taught-developer/',
-				permanent: true,
-			},
-			{
-				source: '/4-common-struggles-of-developers-and-how-to-face-those/',
-				destination:
-					'/posts/4-common-struggles-of-developers-and-how-to-face-those/',
-				permanent: true,
-			},
-			{
-				source: '/5-common-mistakes-to-avoid-when-learning-to-code/',
-				destination: '/posts/5-common-mistakes-to-avoid-when-learning-to-code/',
-				permanent: true,
-			},
-			{
-				source: '/dev-diary-1-journey-to-fullstack-development-pt-1/',
-				destination:
-					'/posts/dev-diary-1-journey-to-fullstack-development-pt-1/',
-				permanent: true,
-			},
-		]
+		return redirections
 	},
 	trailingSlash: true,
 }
